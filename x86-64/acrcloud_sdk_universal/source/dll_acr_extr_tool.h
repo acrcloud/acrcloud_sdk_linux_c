221265d3636b016daa9e8a705634eae4d9412a5f
pony <qiuxuewen@gmail.com>
2024-10-17 15:47:37 +0800

    update libacrcloud_extr_tool.so

--- x86-64/acrcloud_sdk_universal/source/dll_acr_extr_tool.h
+++ x86-64/acrcloud_sdk_universal/source/dll_acr_extr_tool.h
@@ -5,41 +5,152 @@
 extern "C" {
 #endif
 
-#ifdef WIN32 //windows platform
- 
-#ifdef ACR_USER_MODULE_EXPORTS
-#define ACR_API __declspec(dllexport)
-#else
-#define ACR_API __declspec(dllimport)
-#endif
- 
-#ifndef ACR_CALL_TYPE
-#define ACR_CALL_TYPE  	__stdcall  
-#endif
- 
-#else //linux platform
- 
 #ifndef ACR_API
-#define ACR_API __attribute__ ((visibility ("default")))
-#endif
- 
-#ifndef ACR_CALL_TYPE
-#define ACR_CALL_TYPE
-#endif
- 
+#define ACR_API __attribute__((visibility("default")))
 #endif
 
-ACR_API int create_fingerprint(char *pcm_buffer, int pcm_buffer_len, char is_db_fingerprint, char **fps_buffer);
-ACR_API int create_fingerprint_by_file(char *file_path, int start_time_seconds, int audio_len_seconds, char is_db_fingerprint, char **fps_buffer);
-ACR_API int create_fingerprint_by_filebuffer(char *file_buffer, int file_buffer_len, int start_time_seconds, int audio_len_seconds, char is_db_fingerprint, char **fps_buffer);
+/**
+ *  create audio fingerprint from pcm buffer
+ *
+ *  @param pcm_buffer: audio data buffer(RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16
+ *bit, mono 8000 Hz);
+ *  @param pcm_buffer_len: bytes of pcm_buffer
+ *  @param is_db_fingerprint: If it is True, it will create db frigerprint (Fingerprint for bucket,
+ *not for recognition);
+ *
+ *  @param fps_buffer: return fingerprint, need "acr_free" to free this memory.
+ **/
+ACR_API int create_fingerprint(char  *pcm_buffer,
+                               int    pcm_buffer_len,
+                               char   is_db_fingerprint,
+                               char **fps_buffer);
+
+/**
+ *  create fingerprint from file path.
+ *  Audio: mp3, wav, m4a, flac, aac, amr, ape, ogg ...
+ *  Video: mp4, mkv, wmv, flv, ts, avi ...
+ *
+ *  @param file_path: file path
+ *  @param start_time_seconds: skip (start_time_seconds) seconds from from the beginning of
+ *(file_path)
+ *  @param audio_len_seconds: Length of audio data you need. if you create recogize frigerprint,
+ *default is 12 seconds, if you create db frigerprint, it is not usefully;
+ *  @param is_db_fingerprint: If it is True, it will create db frigerprint (Fingerprint for bucket,
+ *not for recognition);
+ *
+ *  @param fps_buffer: return fingerprint, need "acr_free" to free this memory.
+ **/
+ACR_API int create_fingerprint_by_file(char  *file_path,
+                                       int    start_time_seconds,
+                                       int    audio_len_seconds,
+                                       char   is_db_fingerprint,
+                                       char **fps_buffer);
+
+/**
+ *  create fingerprint from file buffer.
+ *  Audio: mp3, wav, m4a, flac, aac, amr, ape, ogg ...
+ *  Video: mp4, mkv, wmv, flv, ts, avi ...
+ *
+ *  @param file_buffer: file buffer
+ *  @param file_buffer_len: length of file_buffer
+ *  @param start_time_seconds: skip (start_time_seconds) seconds from from the beginning of
+ *(file_path)
+ *  @param audio_len_seconds: Length of audio data you need. if you create recogize frigerprint,
+ *default is 12 seconds, if you create db frigerprint, it is not usefully;
+ *  @param is_db_fingerprint: If it is True, it will create db frigerprint (Fingerprint for bucket,
+ *not for recognition);
+ *
+ *  @param fps_buffer: return fingerprint, need "acr_free" to free this memory.
+ **/
+ACR_API int create_fingerprint_by_filebuffer(char  *file_buffer,
+                                             int    file_buffer_len,
+                                             int    start_time_seconds,
+                                             int    audio_len_seconds,
+                                             char   is_db_fingerprint,
+                                             char **fps_buffer);
+
 ACR_API int create_fingerprint_by_fpbuffer(char *fp_buffer, int fp_buffer_len, int start_time_seconds, int len_seconds, char **fps_buffer);
-ACR_API int create_humming_fingerprint(char *pcm_buffer, int pcm_buffer_len, char **fps_buffer);
-ACR_API int create_humming_fingerprint_by_file(char *file_path, int start_time_seconds, int audio_len_seconds, char **fps_buffer);
-ACR_API int create_humming_fingerprint_by_filebuffer(char *file_buffer, int file_buffer_len, int start_time_seconds, int audio_len_seconds, char **fps_buffer);
-ACR_API int decode_audio_by_file(char *file_path, int start_time_seconds, int audio_len_seconds, char **audio_buffer);
-ACR_API int decode_audio_by_filebuffer(char *file_buffer, int file_buffer_len, int start_time_seconds, int audio_len_seconds, char **audio_buffer);
+
+/**
+ *  create humming fingerprint from file path.
+ *  Audio: mp3, wav, m4a, flac, aac, amr, ape, ogg ...
+ *  Video: mp4, mkv, wmv, flv, ts, avi ...
+ *
+ *  @param file_path: file path
+ *  @param start_time_seconds: skip (start_time_seconds) seconds from from the beginning of
+ *(file_path)
+ *  @param audio_len_seconds: Length of audio data you need. if you create recogize frigerprint,
+ *default is 12 seconds;
+ *
+ *  @param fps_buffer: return humming fingerprint, need "acr_free" to free this memory.
+ **/
+ACR_API int create_humming_fingerprint_by_file(char  *file_path,
+                                               int    start_time_seconds,
+                                               int    audio_len_seconds,
+                                               char **fps_buffer);
+
+/**
+ *  create humming fingerprint from file buffer.
+ *  Audio: mp3, wav, m4a, flac, aac, amr, ape, ogg ...
+ *  Video: mp4, mkv, wmv, flv, ts, avi ...
+ *
+ *  @param file_buffer: file buffer
+ *  @param file_buffer_len: length of file_buffer
+ *  @param start_time_seconds: skip (start_time_seconds) seconds from from the beginning of
+ *(file_path)
+ *  @param audio_len_seconds: Length of audio data you need. if you create recogize frigerprint,
+ *default is 12 seconds;
+ *
+ *  @param fps_buffer: return fingerprint, need "acr_free" to free this memory.
+ **/
+ACR_API int create_humming_fingerprint_by_filebuffer(char  *file_buffer,
+                                                     int    file_buffer_len,
+                                                     int    start_time_seconds,
+                                                     int    audio_len_seconds,
+                                                     char **fps_buffer);
+
+/**
+ *  decode audio buffer (8000hz mono 16bit) from file path.
+ *  Audio: mp3, wav, m4a, flac, aac, amr, ape, ogg ...
+ *  Video: mp4, mkv, wmv, flv, ts, avi ...
+ *
+ *  @param file_path: file path
+ *  @param start_time_seconds: skip (start_time_seconds) seconds from from the beginning of
+ *(file_path)
+ *  @param audio_len_seconds: Length of audio data you need. if you create recogize frigerprint,
+ *default is 12 seconds;
+ *
+ *  @param audio_buffer: return audio buffer, need "acr_free" to free this memory.
+ **/
+ACR_API int decode_audio_by_file(char  *file_path,
+                                 int    start_time_seconds,
+                                 int    audio_len_seconds,
+                                 char **audio_buffer);
+
+/**
+ *  decode audio buffer (8000hz mono 16bit) from file buffer.
+ *  Audio: mp3, wav, m4a, flac, aac, amr, ape, ogg ...
+ *  Video: mp4, mkv, wmv, flv, ts, avi ...
+ *
+ *  @param file_buffer: file buffer
+ *  @param file_buffer_len: bytes of file buffer
+ *  @param start_time_seconds: skip (start_time_seconds) seconds from from the beginning of
+ *(file_path)
+ *  @param audio_len_seconds: Length of audio data you need. if you create recogize frigerprint,
+ *default is 12 seconds;
+ *
+ *  @param audio_buffer: return audio buffer, need "acr_free" to free this memory.
+ **/
+ACR_API int decode_audio_by_filebuffer(char  *file_buffer,
+                                       int    file_buffer_len,
+                                       int    start_time_seconds,
+                                       int    audio_len_seconds,
+                                       char **audio_buffer);
+
 ACR_API int get_duration_ms_by_file(char *file_path);
+ACR_API int get_duration_ms_by_filebuffer(char *file_buffer, int file_buffer_len);
 ACR_API int get_duration_ms_by_fpbuffer(char *fp_buffer, int fp_buffer_len);
+
 ACR_API void acr_free(char *buffer);
 ACR_API void acr_set_debug();
 ACR_API void acr_init();
